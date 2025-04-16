385a94f3f3a34b864681fce1e0294e4ee8fb9439
Patrick Peixoto <patrickpeixoto.web@gmail.com>
2025-04-16 18:58:24 -0300

    Adiciona rotas para gerenciamento de usuários, doações, resgates, categorias e imagens de doações

--- routes/api.php
+++ routes/api.php
@@ -9,5 +9,34 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // });
 
+Route::post('/createUser', [DoaFacilController::class, 'createUser']);
+Route::get('/getUsers', [DoaFacilController::class, 'getUsers']);
+Route::get('/getUser/{id}', [DoaFacilController::class, 'getUserById']);
+Route::get('/validateUser', [DoaFacilController::class, 'validateUser']);
+Route::put('/updateUser/{id}', [DoaFacilController::class, 'updateUser']);
+Route::delete('/deleteUser/{id}', [DoaFacilController::class, 'deleteUser']);
+
 Route::get('/donations', [DoaFacilController::class, 'donations']);
-Route::post('/register', [DoaFacilController::class, 'register']);
+Route::get('/donations/{id}', [DoaFacilController::class, 'getDonationById']);
+Route::post('/createDonation', [DoaFacilController::class, 'createDonation']);
+Route::put('/updateDonation/{id}', [DoaFacilController::class, 'updateDonation']);
+Route::delete('/deleteDonation/{id}', [DoaFacilController::class, 'deleteDonation']);
+Route::get('/donationsFiltered', [DoaFacilController::class, 'getDonationsByFilter']);
+
+Route::get('/rescues', [DoaFacilController::class, 'getRescues']);
+Route::get('/rescuesFiltered', [DoaFacilController::class, 'getRescueByFilter']);
+Route::post('/createRescue', [DoaFacilController::class, 'createRescue']);
+Route::put('/updateRescue/{id}', [DoaFacilController::class, 'updateRescue']);
+Route::delete('/deleteRescue/{id}', [DoaFacilController::class, 'deleteRescue']);
+
+Route::post('createCategory', [DoaFacilController::class, 'createCategory']);
+Route::get('categories', [DoaFacilController::class, 'getCategories']);
+Route::get('categories/{id}', [DoaFacilController::class, 'getCategoryById']);
+Route::put('updateCategory/{id}', [DoaFacilController::class, 'updateCategory']);
+Route::delete('deleteCategory/{id}', [DoaFacilController::class, 'deleteCategory']);
+
+Route::post('createDonationImage', [DoaFacilController::class, 'createDonationImage']);
+Route::get('donationImages', [DoaFacilController::class, 'getDonationImages']);
+Route::get('donationImages/{id}', [DoaFacilController::class, 'getDonationImageById']);
+Route::put('updateDonationImage/{id}', [DoaFacilController::class, 'updateDonationImage']);
+Route::delete('deleteDonationImage/{id}', [DoaFacilController::class, 'deleteDonationImage']);
